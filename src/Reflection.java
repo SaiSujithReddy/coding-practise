71a5449f101c3d6dd7ccf2e4c6f3af97155ead93
Sai MarapaReddy <sai.marapareddy@gmail.com>
2016-11-22 22:50:13 -0800

    Adding reflection java class

---
+++ src/Reflection.java
@@ -0,0 +1,42 @@
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Collections;
+
+/**
+ * Created by sai on 11/22/16.
+ */
+class Student{
+    private String name;
+    private String id;
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void anothermethod(){  }
+
+}
+
+public class Reflection {
+
+    public static void main(String[] args){
+        Class student = Student.class;
+        Method[] methods = student.getDeclaredMethods();
+
+        ArrayList<String> methodList = new ArrayList<>();
+        for(Method method : methods){
+            methodList.add(method.getName());
+        }
+        Collections.sort(methodList);
+        for(String name: methodList){
+            System.out.println(name);
+        }
+    }
+
+}
