becf3d313af29ab1ffb14e655702bf53ab1aa7d2
Sai MarapaReddy <sai.marapareddy@gmail.com>
2016-11-24 14:32:56 -0800

    Adding corner case to AppleStockProgram

--- src/hackerrank/Applestock.java
+++ src/hackerrank/Applestock.java
@@ -10,24 +10,28 @@ public class Applestock {
         int probable_buy = arg[0];
         int sell = arg[1];
         int probable_sell = arg[1];
+        int probable_buy_index = 0;
+        int probable_sell_index = 0;
         for(int i=1; i<arg.length;i++) {
-
             if (arg[i] < buy ) {
                 probable_buy = arg[i];
+                probable_buy_index = i;
             }
             if (arg[i] > sell ) {
                 probable_sell = arg[i];
+                probable_sell_index = i;
             }
             if((probable_sell - probable_buy) > (sell - buy )) {
-                sell = probable_sell;
-                buy = probable_buy;
+                if (probable_sell_index > probable_buy_index) {
+                    sell = probable_sell;
+                    buy = probable_buy;
+                }
             }
         }
-
         return new int[]{buy,sell};
     }
     public static void main(String[] args) {
-        int[] stockPricesYesterday = new int[]{1,1, 5, 5, 8, 10, 19};
+        int[] stockPricesYesterday = new int[]{17,18, 24, 25, 8, 10, 17};
         for (int i : myFunction(stockPricesYesterday)) {
             System.out.println(i);
         }
